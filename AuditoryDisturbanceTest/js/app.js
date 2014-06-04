6bbef7722f7f7605c99f1bacc942e6e1ab28529a
Kristian Koivisto <kristiak@cs.helsinki.fi>
2014-06-04 14:16:48 +0300

    now doesn't play disturbance sounds forever

--- AuditoryDisturbanceTest/js/app.js
+++ AuditoryDisturbanceTest/js/app.js
@@ -5,8 +5,9 @@
 
 function playTestSounds() {
 
-    var numberSequence = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1, 9, 8, 7, 6, 5];
+    var numberSequence = [ 1, 2, 3, 4, 5, 6, 8, 9, 1, 2, 3, 4, 6, 7, 8, 9, 1, 2, 3, 4, 6, 7, 8, 9];
     numberSequence.index = 0;
+    var sequenceExhausted = false;
 
     var delaysBetweenNumbers = [100,125,150,175,200];
 
@@ -19,22 +20,28 @@ function playTestSounds() {
                 setTimeout (function () {
                     numberSounds[numberSequence[numberSequence.index] - 1].play();
                 }, delaysBetweenNumbers[randomDelayIndex]);
+            } else {
+                sequenceExhausted = true;
             }
         };
         numberSounds[numberSequence[0]-1].play();
     }
 
-    playMusicSound = function () {
-        var wordOrMusic = Math.floor(Math.random()*2);
-        if (wordOrMusic == 0 ) {
-            var whichWord = Math.floor(Math.random()*wordSounds.length);
-            wordSounds[whichWord].play();
+    playDisturbanceSound = function () {
+        if (sequenceExhausted == false) {
+            var wordOrMusic = Math.floor(Math.random()*2);
+            if (wordOrMusic == 0) {
+                var whichWord = Math.floor(Math.random()*wordSounds.length);
+                wordSounds[whichWord].play();
+            } else {
+                var whichMusic = Math.floor(Math.random()*musicSounds.length);
+                musicSounds[whichMusic].play();
+            }
         } else {
-            var whichMusic = Math.floor(Math.random()*musicSounds.length);
-            musicSounds[whichMusic].play();
+            clearInterval(disturbance);
         }
-    }
+    };
 
-    setInterval (playMusicSound, 4000);
+    var disturbance = setInterval (playDisturbanceSound, 4000);
 
 }
